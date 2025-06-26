e17a0c0753addc5e501bc1298473fff903253ec1
John Shaffer <jdsha@pm.me>
2025-06-26 09:18:20 -0500

    Add update-wordpress output

--- flake.nix
+++ flake.nix
@@ -53,8 +53,98 @@
               sha256 = data.sha256;
             };
           }))) version_data;
+        latest = versions.wordpress_6_8_1;
+        # write a bash script as a binary
+        update-wordpress = writeShellApplication {
+          name = "update-wordpress";
+          runtimeInputs = [ ];
+
+          text = ''
+            usage() {
+              cat <<EOF
+            Usage: $0 [OPTIONS] [WPDIR]
+
+            Options:
+              -s, --src DIR     Path to the source WordPress files (default: ${latest}/share/wordpress)
+              -h, --help        Show this help message and exit
+            EOF
+            }
+
+            if ! OPTS=$(getopt -o s:w:h --long src:,wp:,help -n 'parse-options' -- "$@"); then
+              usage >&2
+              exit 1
+            fi
+
+            eval set -- "$OPTS"
+
+            SRCDIR=""
+            WPDIR=""
+
+            while true; do
+              case "$1" in
+                -s|--src)
+                  SRCDIR="$2"
+                  shift 2
+                  ;;
+                -h|--help)
+                  usage
+                  exit 0
+                  ;;
+                --)
+                  shift
+                  break
+                  ;;
+                *)
+                  echo "Unknown option: $1" >&2
+                  usage >&2
+                  exit 1
+                  ;;
+              esac
+            done
+
+            if [ -z "$SRCDIR" ]; then
+              SRCDIR="${latest}/share/wordpress"
+            fi
+
+            if [ $# -gt 0 ]; then
+              WPDIR="$1"
+            fi
+
+            if [ -z "$WPDIR" ]; then
+              WPDIR="$(pwd)"
+            fi
+
+            cd "$WPDIR"
+
+            if [ ! -f "wp-config.php" ] && [ ! -f "wp-config-sample.php" ]; then
+              echo "Error: '$WPDIR' does not contain a WordPress installation." >&2
+              exit 1
+            fi
+
+            echo "Using SRCDIR: $SRCDIR"
+            echo "Using WPDIR: $WPDIR"
+
+            TMPDIR=$(mktemp -d)
+
+            cp -r "$SRCDIR"/wp-includes "$SRCDIR"/wp-admin "$SRCDIR"/wp-content "$SRCDIR"/*.* "$TMPDIR"
+            chmod ug+w -R "$TMPDIR"
+
+            set -o noclobber
+            true > .maintenance
+            set +o noclobber
+            rm -rf wp-includes wp-admin
+
+            cp -r "$TMPDIR"/* .
+
+            rm .maintenance
+          '';
+        };
       in {
         devShells.default = mkShell { buildInputs = [ omnix ]; };
-        packages = versions // { default = versions.wordpress_6_8_1; };
+        packages = versions // {
+          default = latest;
+        } // {
+          inherit update-wordpress;
+        };
       });
 }
