73da909ba6cf8c108da7d6b294a0e23b97ba2913
John Shaffer <jdsha@pm.me>
2025-07-08 12:39:07 -0500

    mkWpConfig -> mkWPConfig

--- src/lib.nix
+++ src/lib.nix
@@ -1,7 +1,7 @@
 let WPConfigFormat = ./formats/wp-config;
 in {
   inherit WPConfigFormat;
-  mkWpConfig = { pkgs, lib, name, settings }:
+  mkWPConfig = { pkgs, lib, name, settings }:
     let
       settingsFormat = (import WPConfigFormat { inherit pkgs lib; }).format { };
     in settingsFormat.generate name settings;
