22aee6812249c1ee2b7f93e3207a68002a9db3cb
John Shaffer <jdsha@pm.me>
2025-12-26 15:22:26 -0600

    Remove flake-utils dependency
    It's not needed since we can easily implement
    the functionality ourselves.
    
    See https://ayats.org/blog/no-flake-utils

--- flake.nix
+++ flake.nix
@@ -3,64 +3,72 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      with import nixpkgs { inherit system; };
-      with pkgs;
-      let
-        ourLib = import ./src/lib.nix;
-        mkWPUpdater =
-          wordpress-source:
-          (ourLib.mkWPUpdater {
-            inherit
-              lib
-              pkgs
-              replaceVarsWith
-              wordpress-source
-              ;
-          });
-        versions = lib.attrsets.mapAttrs (
-          name: data:
-          (import ./src/wordpress.nix {
-            inherit (pkgs) lib stdenv fetchzip;
-            version = data.version;
-            hash = data.hash;
-          })
-        ) (lib.importJSON (self + "/wordpress-versions.json"));
-        latest = versions.wordpress_6_9_0;
-        updaters = lib.attrsets.mapAttrs' (
-          name: wordpress-source: lib.attrsets.nameValuePair ("update-" + name) (mkWPUpdater wordpress-source)
-        ) versions;
-      in
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            jq
-            jsonfmt
-            nix
-            omnix
-          ];
-        };
-        lib = ourLib;
-        packages =
-          versions
-          // {
-            default = latest;
-          }
-          // updaters
-          // {
-            update-wordpress = mkWPUpdater latest;
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs supportedSystems (system: function nixpkgs.legacyPackages.${system});
+      ourLib = import ./src/lib.nix;
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              jq
+              jsonfmt
+              nix
+              omnix
+            ];
           };
-      }
-    );
+      });
+      lib = ourLib;
+      packages = forAllSystems (
+        pkgs:
+        with pkgs;
+        let
+          mkWPUpdater =
+            wordpress-source:
+            (ourLib.mkWPUpdater {
+              inherit
+                lib
+                pkgs
+                replaceVarsWith
+                wordpress-source
+                ;
+            });
+          versions = lib.attrsets.mapAttrs (
+            name: data:
+            (import ./src/wordpress.nix {
+              inherit (pkgs) lib stdenv fetchzip;
+              version = data.version;
+              hash = data.hash;
+            })
+          ) (lib.importJSON (self + "/wordpress-versions.json"));
+          latest = versions.wordpress_6_9_0;
+          updaters = lib.attrsets.mapAttrs' (
+            name: wordpress-source: lib.attrsets.nameValuePair ("update-" + name) (mkWPUpdater wordpress-source)
+          ) versions;
+        in
+        versions
+        // {
+          default = latest;
+        }
+        // updaters
+        // {
+          update-wordpress = mkWPUpdater latest;
+        }
+      );
+    };
 }
