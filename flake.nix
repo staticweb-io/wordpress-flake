9e35cc62f12181fbfaec1dad7f60493dd9fd9972
John Shaffer <jdsha@pm.me>
2026-03-11 11:24:08 -0500

    Add WordPress 6.9.4

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
               hash = data.hash;
             })
           ) (lib.importJSON (self + "/wordpress-versions.json"));
-          latest = versions.wordpress_6_9_2;
+          latest = versions.wordpress_6_9_4;
           updaters = lib.attrsets.mapAttrs' (
             name: wordpress-source: lib.attrsets.nameValuePair ("update-" + name) (mkWPUpdater wordpress-source)
           ) versions;
